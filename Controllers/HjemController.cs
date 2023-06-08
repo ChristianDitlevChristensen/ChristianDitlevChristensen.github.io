14cd0e8ed469e0195e90269388f66f5152759c3c
Christian Ditlev Christensen <76095431+ChristianDitlevChristensen@users.noreply.github.com>
2023-06-08 10:37:49 +0200

    Add files via upload

---
+++ Controllers/HjemController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Vildmosemanden.Models;
+
+namespace Vildmosemanden.Controllers
+{
+    public class HjemController : Controller
+    {
+        private readonly ILogger<HjemController> _logger;
+
+        public HjemController(ILogger<HjemController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Vildmosemanden()
+        {
+            return View();
+        }
+
+        public IActionResult Sagnet()
+        {
+            return View();
+        }
+
+        public IActionResult Sang()
+        {
+            return View();
+        }
+
+        public IActionResult Skål()
+        {
+            return View();
+        }
+
+        public IActionResult HvadSigerVildmosemanden()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
